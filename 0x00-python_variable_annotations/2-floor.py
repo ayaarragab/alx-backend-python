1234f1e4886c25d61bf87fae50fa0bbb8d7616f3
ayaarragab <ayarsk4343@gmail.com>
2024-07-06 21:55:41 +0300

    task 1

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+a type-annotated function concat that takes
+floor which takes a float n as argument
+and returns the floor of the float
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    a type-annotated function concat that takes
+    floor which takes a float n as argument
+    and returns the floor of the float
+    """
+
+    return math.floor(n)

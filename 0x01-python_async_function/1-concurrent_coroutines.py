7ef74912305bb0c55623013a9feb40f0756af5d5
ayaarragab <ayarsk4343@gmail.com>
2024-07-10 03:14:28 +0300

    1. Let's execute multiple coroutines at the same time with async

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,30 @@
 #!/usr/bin/env python3
+"""
+an async routine called wait_n that takes in 2
+int arguments (in this order): n and max_delay.
+You will spawn wait_random n times with the specified
+max_delay. wait_n should return the list of all
+the delays (float values).
+"""
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    an async routine called wait_n that takes in 2
+    int arguments (in this order): n and max_delay.
+    You will spawn wait_random n times with the specified
+    max_delay. wait_n should return the list of all
+    the delays (float values).
+    """
+    stored_list: List[float] = []
+    for _ in range(0, n):
+        stored_list.append(await wait_random(max_delay))
+    for i, _ in enumerate(stored_list):
+        minimum = min(stored_list[i:])
+        min_index = stored_list.index(minimum)
+        temp = stored_list[i]
+        stored_list[i] = minimum
+        stored_list[min_index] = temp
+    return stored_list

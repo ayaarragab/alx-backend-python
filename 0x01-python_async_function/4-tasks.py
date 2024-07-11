fba69d9a0b1ccd86b1e6b36e4d2d210ba120d12b
ayaarragab <ayarsk4343@gmail.com>
2024-07-11 09:04:59 +0300

    Yarab a3raf a7el task4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+"""
+Doc for task_wait_n
+"""
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+# from asyncio import run
+
+
+def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Doc for task_wait_n
+    """

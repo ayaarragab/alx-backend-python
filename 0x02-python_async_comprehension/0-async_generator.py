3ed421faeaf105e7b8278b13354aa727aa9ca1c4
ayaarragab <ayarsk4343@gmail.com>
2024-07-11 09:40:18 +0300

    task 0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+a coroutine called async_generator
+that takes no arguments
+"""
+import random
+from asyncio import sleep
+from typing import AsyncIterator
+
+
+async def async_generator() -> AsyncIterator[float]:
+    """
+    a coroutine called async_generator
+    that takes no arguments
+    """
+    for i in range(0, 10):
+        await sleep(1)
+        yield random.uniform(0, 10)

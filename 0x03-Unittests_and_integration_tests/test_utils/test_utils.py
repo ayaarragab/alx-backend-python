e0796c3fe61cdb236ed4fdd2a6100b3cf227ba8c
ayaarragab <ayarsk4343@gmail.com>
2024-08-26 10:37:47 +0300

    1st task

---
+++ 0x03-Unittests_and_integration_tests/test_utils/test_utils.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+unittests for Generic
+utilities for github org client.
+"""
+from unittest import TestCase, main
+from parameterized import parameterized
+from utils import access_nested_map
+from typing import (
+    Mapping,
+    Sequence,
+    Any,
+    Dict,
+    Callable,
+)
+
+
+class TestAccessNestedMap(TestCase):
+    """
+    TestAccessNestedMap class that inherits from unittest.TestCase.
+    """
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """Test access_nested_map method."""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Test access_nested_map method raises KeyError."""
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
+
+
+if __name__ == "__main__":
+    main()
